43c38f5afbc36b5cfa2f1535dbdcfb1837ae0fdf
Karolina Kulpinska <kulpinska.karolinaa@gmail.com>
2025-11-18 16:06:47 +0100

    Adding the UseCurrentDate file

--- src/App/Clock/index.js
+++ src/App/Clock/index.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Wrapper } from "./styled";
+import { useCurrentDate } from "../useCurrentDate";
 
 const formatDate = (date) => date.toLocaleString(undefined, {
     weekday: "long",
@@ -13,17 +14,7 @@ const formatDate = (date) => date.toLocaleString(undefined, {
 });
 
 export const Clock = () => {
-    const [date, setDate] = useState(new Date());
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setDate(new Date());
-        }, 1000);
-
-        return () => {
-            clearInterval(intervalId);
-        };
-    }, []);
+    const date = useCurrentDate();
 
     return (
         <Wrapper>
